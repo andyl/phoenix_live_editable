ff168adc668e2833b17a4ad7993146bd5c89033c
andyl <andy@r210.com>
2019-09-24 07:47:25 -0700

    Update project name

--- mix.exs
+++ mix.exs
@@ -1,24 +1,21 @@
-defmodule LiveEditable.MixProject do
+defmodule Phoenix.LiveEditable.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :live_editable,
-      version: "0.1.0",
+      app: :phoenix_live_editable,
+      version: "0.0.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:phoenix_html, "~> 2.11"},

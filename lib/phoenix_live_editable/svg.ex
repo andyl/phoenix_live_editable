bb465c67ee79f6f068021c3c29956d0921ef7642
andyl <andy@r210.com>
2020-01-23 13:12:00 -0800

    Remove fonts

---
+++ lib/phoenix_live_editable/svg.ex
@@ -0,0 +1,14 @@
+defmodule PhoenixLiveEditable.Svg do
+  defmacro inline(file) do
+    path = static_path(file)
+    case File.read(path) do
+      {:ok, text} -> quote do: {:safe, unquote(text)}
+      {:error, _} -> raise "No SVG for #{path}"
+    end
+  end
+
+  defp static_path(file_name) do
+    path = "#{__DIR__}/../../assets/static/svg"
+    [path, "#{file_name}.svg"] |> Path.join() |> Path.expand 
+  end
+end

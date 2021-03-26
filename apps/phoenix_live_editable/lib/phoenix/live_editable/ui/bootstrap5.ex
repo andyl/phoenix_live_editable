f2c7e06d80bfcc1c2698aff592d547737b5895ec
andyl <andy@r210.com>
2021-03-26 07:25:43 -0700

    Add target option

--- apps/phoenix_live_editable/lib/phoenix/live_editable/ui/bootstrap5.ex
+++ apps/phoenix_live_editable/lib/phoenix/live_editable/ui/bootstrap5.ex
@@ -16,8 +16,11 @@ defmodule Phoenix.LiveEditable.Ui.Bootstrap5 do
   """
   @impl Ui.Base
   def form_text(label, opts) do
+    target = if opts[:target], do: "phx-target='#{opts[:target]}'", else: ""
+    change = "phx-change='#{opts[:on_change]}'"
+    submit = "phx-submit='#{opts[:on_submit]}'"
     """
-    <form class="form-inline" phx-change="#{opts[:on_change]}" phx-submit="#{opts[:on_submit]}">
+    <form class="form-inline" #{change} #{submit} #{target}>
     <div class="mb-2 form-group">
       <input type="text" class="form-control" name="editable_text" value="#{label}">
     </div>
@@ -39,8 +42,10 @@ defmodule Phoenix.LiveEditable.Ui.Bootstrap5 do
   def form_select(label, opts) do
     Keyword.has_key?(opts, :options) || raise("Needs `:options` option")
     options = Enum.map(opts[:options], &("<option value='#{elem(&1,0)}'>#{elem(&1,1)}</option>"))
+    target = if opts[:target], do: "phx-target='#{opts[:target]}'", else: ""
+    submit = "phx-submit='#{opts[:on_submit]}'"
     """
-    <form class="form-inline" phx-submit="#{opts[:on_submit]}">
+    <form class="form-inline" #{target} #{submit}>
     <div class="form-group col-md-6">
       <select name="editable_select" class="form-control" value="#{label}">
       #{Enum.join(options, "")}

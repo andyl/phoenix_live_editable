cd8735ede9495af9182e2f4cb70188a0aadb8efb
andyl <andy@r210.com>
2022-12-17 17:00:04 -0800

    Fix controller tests

--- apps/ple_demo_tailwind3/test/ple_demo_tailwind3_web/controllers/page_controller_test.exs
+++ apps/ple_demo_tailwind3/test/ple_demo_tailwind3_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule PleDemoTailwind3Web.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Tailwind"
   end
 end

5fc50fc9f2b1674ab9f9ce333bb52014889737bd
andyl <andy@r210.com>
2022-12-18 19:32:52 -0800

    Refactor LiveEditableView

---
+++ apps/phoenix_live_editable/lib/phoenix/editable/handler/default.ex
@@ -0,0 +1,35 @@
+defmodule Phoenix.Editable.Handler.Default do
+  @moduledoc """
+  Default Handlers for Views and Phoenix.Editable.Base
+
+  use Phoenix.LiveEditable.Handler.Default
+  """
+
+  defmacro __using__(_opts) do
+    quote do
+
+      # needed for 'assign'
+      use Phoenix.Component
+
+      def handle_event("ple-blur", data, socket) do
+        {:noreply, assign(socket, :ple_mode, "anchor")}
+      end
+
+      def handle_event("ple-focus", data, socket) do
+        {:noreply, assign(socket, :ple_mode, "focus")}
+      end
+
+      def handle_event("ple-default-save", data, socket) do
+        {:noreply, assign(socket, ple_data: data["data"], ple_mode: "anchor")}
+      end
+
+      def handle_event(target, data, socket) do
+        IO.puts("XXXXX -- UNHANDLED EVENT -- XXXXX")
+        IO.inspect(target)
+        IO.inspect(data)
+        IO.puts("XXXXX -- fini -- XXXXX")
+        {:noreply, socket}
+      end
+    end
+  end
+end
